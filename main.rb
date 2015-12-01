bbec87a486da847e65ef7cb42f3b7eb7b766417c
John T Skarbek <jtslear@gmail.com>
2015-11-30 21:58:07 -0500

    wip

--- main.rb
+++ main.rb
@@ -9,11 +9,8 @@ def herb(template, options={}, locals={})
 end
 
 
-JOHN= "5313 Echo Ridge Road Raleigh, NC 27612"
 RWORK="2810 Meridian Parkway #176, Durham, NC 27713"
-EWORK="434 Fayetteville St #1120, Raleigh, NC 27601"
-PETCO_1="6231 Triangle Plantation Dr Raleigh, NC 27616"
-PETCO_2="7811 Alexander Promenade Pl Raleigh, NC 27617"
+JWORK="1101 Haynes St #105, Raleigh, NC 27604"
 
 get '/' do
   if params[:site]
@@ -25,16 +22,10 @@ get '/' do
 
   if @address
     address = @address
-    john = DistanceFinder.new(address, JOHN)
     rwork = DistanceFinder.new(address, RWORK)
-    ework = DistanceFinder.new(address, EWORK)
-    petco_1 = DistanceFinder.new(address, PETCO_1)
-    petco_2 = DistanceFinder.new(address, PETCO_2)
-    @john_time = john.time
+    jwork = DistanceFinder.new(address, JWORK)
     @rwork_time = rwork.time
-    @ework_time = ework.time
-    @petco_1_time = petco_1.time
-    @petco_2_time = petco_2.time
+    @jwork_time = jwork.time
   end
 
   herb :index
